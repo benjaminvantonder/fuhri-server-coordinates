93e977858d6606bc6d7cc0e0b38b68ea91e05777
jsmaniac0 <vissievanafrika@gmail.com>
2025-09-19 15:09:08 +0200

    home portal

--- src/data/sampleCoordinates.ts
+++ src/data/sampleCoordinates.ts
@@ -14,4 +14,17 @@ export const sampleCoordinates: Coordinate[] = [
     discoveredBy: "Nicolas Alberts",
     dateDiscovered: "2025-09-19",
   },
+  {
+    id: "2",
+    name: "Community Nether Portal",
+    description: "Portal by almal se bases",
+    x: 80,
+    y: 65,
+    z: -498,
+    dimension: "overworld",
+    type: "man-made",
+    tags: ["Portal", "Safe", "Overworld"],
+    discoveredBy: "Almal",
+    dateDiscovered: "2025-09-19",
+  },
 ];
